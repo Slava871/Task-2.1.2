61be490ac4181b8efb739ee0c3147225dfbf232c
Slava <loginmail2018@inbox.ru>
2022-09-18 14:43:54 +0300

    +

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,23 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
+        HelloWorld bean1 =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+
+
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+
+        System.out.println(bean1 == bean2);
+
+
+        Cat bean_cat1 =
+                (Cat) applicationContext.getBean("cat");
+
+        Cat bean_cat2 =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(bean_cat1 == bean_cat2);
+
+
     }
 }
